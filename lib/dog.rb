4aeca43de82b7168dcac3d62ad01181ee6ad6731
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-08 22:03:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
   def name=(dog_name)
-    @dog_name = dog_name
+    @name = dog_name
   end
   def name
-    @dog_name
+    @name
   end
   def breed=(dog_breed)
-    @dog_breed = dog_breed
+    @breed = dog_breed
   end
   def breed
-    @dog_breed
+    @breed
   end
 end
